c1dd0438e415c73a5d5fa6f3cb4cd883f25be1a8
Mira Estil <mira@Miras-MacBook-Pro.local>
2017-12-06 20:47:34 -0500

    Basic functionality

--- iVoice/ViewController.swift
+++ iVoice/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton! {
+        didSet {
+            recordButton.isEnabled = true
+        }
+    }
+    
+    @IBOutlet weak var stopRecordingButton: UIButton! {
+        didSet {
+            stopRecordingButton.isEnabled = false
+        }
+    }
+    
+    @IBOutlet weak var recordingLabel: UILabel! {
+        didSet {
+            recordingLabel.text = "Tap to Record"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    // MARK: UTILITIES
+    @IBAction func recordAudio(_ sender: UIButton) {
+        recordingLabel.text = "Recording.."
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
 }
 
