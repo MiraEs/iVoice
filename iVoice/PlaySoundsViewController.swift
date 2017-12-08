4e9628d92d0c95339a581680e40c3eb972e81d42
Mira Estil <mira@Miras-MacBook-Pro.local>
2017-12-08 00:48:50 -0500

    sounds yay

--- iVoice/PlaySoundsViewController.swift
+++ iVoice/PlaySoundsViewController.swift
@@ -7,41 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
+
+enum ButtonType: Int {
+    //tags labeled in storyboard to id buttons
+    case slow = 0, fast, chipmunk, vader, echo, reverb
+}
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: URL!
     @IBOutlet weak var slowButton: UIButton!
     @IBOutlet weak var fastButton: UIButton!
     @IBOutlet weak var highPitchButton: UIButton!
     @IBOutlet weak var lowPitchButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
     // MARK: Action
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
         print("play sound")
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
         print("stop button")
+        stopAudio()
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
